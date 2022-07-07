095e37bbeca775068b3338cc52a0226792c3b193
Fernando Bosco <fbosco@dlocal.com>
2022-07-06 22:27:49 -0300

    fix in hardhat config

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -11,9 +11,5 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`],
-    },
   },
 };
